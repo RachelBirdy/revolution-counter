d5d7bc5e22c5f1750583fdd0a897c2b256dc0a22
Rachel Birdy <rachel@weird.tf>
2021-08-16 20:57:14 +0100

    Tweaks

--- pollRateCalculator.c
+++ pollRateCalculator.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdio.h>
 
 int calculatePollRate(int maxRPM) {
 	double secondsPerRev = 60 / maxRPM;
--- pollRateCalculator.h
+++ pollRateCalculator.h
@@ -1,3 +1,4 @@
 #include <math.h>
+#include <stdio.h>
 
 int pollRateCalculator(int maxRPM);
