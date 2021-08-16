ff447d44416b69976d0e984f02c94bb0792a0512
Rachel Birdy <rachel@weird.tf>
2021-08-16 19:39:51 +0100

    This branch is basically going to be a rewrite

---
+++ interface.c
@@ -0,0 +1,9 @@
+#include "pollRateCalculator.c"
+
+#define MAX_RPM 1000
+
+int main() {
+	calculatePollRate(1000);
+	return 0;
+
+}
---
+++ pollRateCalculator.c
@@ -0,0 +1,7 @@
+#include <math.h>
+
+int calculatePollRate(int maxRPM) {
+	double secondsPerRev = 60 / maxRPM;
+	printf("This is a test!\n");
+	return 0;
+}
